98d0bd2a629c28a3e2c3429c42ff846429d72ea8
JRebertt <joaorebertt0219@gmail.com>
2023-02-22 01:57:02 -0300

    Update Day

--- src/App.tsx
+++ src/App.tsx
@@ -58,7 +58,7 @@ function App() {
                       className={clsx(
                         "bg-[#252d4a] w-full sm:w-64 flex justify-center items-center border-4 border-[#234668] px-2 py-5 h-10 rounded-lg hover:bg-[#555e7d]"
                       )}
-                      onClick={() => handleAnswerButtonClick( answer.isCorrect)}
+                      onClick={() => handleAnswerButtonClick( answer.isCorrect?)}
                     >
                       {answer.answerText}
                     </button>
