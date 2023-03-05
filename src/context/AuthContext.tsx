bd48b1e7cb6a9d01cb51b0bd40861c6cea531cba
JRebertt <joaorebertt0219@gmail.com>
2023-03-05 02:09:22 -0300

    feat: add firebase auth

---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,30 @@
+import { createContext, useContext } from "react";
+import {
+  GoogleAuthProvider,
+  signInWithPopup,
+  signInWithRedirect,
+  signOut,
+  onAuthStateChanged,
+} from "firebase/auth";
+
+import { auth } from '../lib/firebase'
+
+const AuthContext = createContext();
+
+export const AuthContextProvider = ({ children }) => {
+  const googleSignIn  = ()  =>  {
+    const provider  = new GoogleAuthProvider()
+    signInWithPopup(auth, provider)
+  }
+  return (
+    <AuthContext.Provider 
+    value={{googleSignIn}}
+    >
+      {children}
+    </AuthContext.Provider>;
+  )
+};
+
+export const UseAuth = () => {
+  return useContext(AuthContext);
+};
