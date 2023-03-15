4cda449c4adbd43d7fa1d3ad46305af15d353566
JRebertt <joaorebertt0219@gmail.com>
2023-03-14 21:44:34 -0300

    update feat and fix

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,21 +3,6 @@
 //   totalQuestions: number;
 // }
 
-
-
-// export function Header({ currentQuestion,totalQuestions }: HeaderProps)  {
-//   return (
-//     <div className="w-80 flex flex-col items-center sm:justify-start gap-5">
-//     <div>
-//       <span className="font-medium text-2xl pr-1 select-none">
-//         Perguntas {currentQuestion + 1}
-//       </span>
-//       <span>/{totalQuestions}</span>
-//     </div>
-//     <div className="text-lg font-semibold py-2">
-//       {currentQuestion.questionText}
-//     </div>
-//   </div>
-// );
-//   )
-// }
+export function Header() {
+  return <></>;
+}
